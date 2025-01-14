902e5f742be430adbef370b0d9310149a872dc05
unknown <nadeem.samaali@gmail.com>
2025-01-14 13:59:04 -0500

    Added translate() method + bug fixing

--- generator.py
+++ generator.py
@@ -1,22 +1,28 @@
+from deep_translator import GoogleTranslator
+
+# Function that translates input string into target language. Returns translated text
+def translate(input : str, target_lang : str) -> str :
+    return GoogleTranslator(source = "auto", target = target_lang).translate(input)
+
 # TODO : create a function that will generate a fixed set of vocabulary words on which the exercices
 # of the worksheet will be based upon
-def get_vocab_bank() -> str[list] : 
+def get_vocab_bank() -> list[str] : 
     pass
 
 # TODO : create a function that will generate a definition and grammatical usage guide on each vocabulary
 # word included in the vocabulary set
-def define(vocabulary : str[list]) -> dict :
+def define(vocabulary : list[str]) -> dict :
     pass
 
 # TODO : create a function that will generate a fill in the blank exercice based on a fixed set
 # of vocabulary words
-def fillin_ex(lang : str, level : float, len : int, vocabulary : str[list]) -> str :
+def fillin_ex(lang : str, level : float, len : int, vocabulary : list[str]) -> str :
     pass
 
 # TODO : create a function that will generate a translation exercice
-def translation_ex(lang : str, level : float, len : int, vocabulary : str[list]) -> str :
+def translation_ex(lang : str, level : float, len : int, vocabulary : list[str]) -> str :
     pass
 
 # TODO : create a function which will generate a PDF file worksheet.
-def generate_doc(lang : str, level : float, len : int, vocabulary : str[list]) :
+def generate_doc(lang : str, level : float, len : int, vocabulary : list[str]) :
     pass
