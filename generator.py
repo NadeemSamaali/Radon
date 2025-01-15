05f386ce6462a926bbc0db7846bd1e1d3bead78c
unknown <nadeem.samaali@gmail.com>
2025-01-15 14:18:43 -0500

    Added method get_commonality_score() to compute the commonality of a word

--- generator.py
+++ generator.py
@@ -1,16 +1,45 @@
-from deep_translator import GoogleTranslator
-
-# Nltk imports
 import nltk
-from nltk.corpus import wordnet as wn
 import random
+import pickle
+
+from deep_translator import GoogleTranslator
+from nltk.corpus import brown
+from nltk.corpus import wordnet as wn
+from nltk.probability import FreqDist
 
 nltk.download("wordnet", quiet=True)
 nltk.download("omw-1.4", quiet=True)
 
+# Loading brown corpus data . If files doesn't exit, download it and pickle it in assets
+try :
+    with open("assets/brown_corpus_data.pkl", "rb") as f:
+        freq_dist, total_words = pickle.load(f)
+
+except :
+    # Download the Brown corpus if not already available
+    nltk.download('brown')
+
+    # Precompute the data
+    brown_words = brown.words()
+    freq_dist = FreqDist(brown_words)
+    total_words = len(brown_words)
+
+    # Save precomputed data to a pickle file
+    with open("assets/brown_corpus_data.pkl", "wb") as f:
+        pickle.dump((freq_dist, total_words), f)
+
 # Function that translates input string into target language. Returns translated text
 def translate(input : str, target_lang : str) -> str :
     return GoogleTranslator(source = "auto", target = target_lang).translate(input)
+
+# Function that assigns a score to a word based on its commonality / difficulty
+def get_commonality_score(word : str) -> float :
+    word = word.lower()
+    # Generate the frequency distribution of words in the chosen corpus
+    word_frequency = freq_dist[word]
+    commonality_score = word_frequency / total_words if word_frequency > 0 else 0
+    
+    return commonality_score
 
 # Function that generates a list of N english words (nouns, adjectives, verbs)
 def get_random_words(pos : str, count : int) :
