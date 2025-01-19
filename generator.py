dc466ecf3cb3a61fd40a9502cacf62f624d43ee0
unknown <nadeem.samaali@gmail.com>
2025-01-18 23:45:59 -0500

    Added scrambled translation exercice

--- generator.py
+++ generator.py
@@ -5,6 +5,8 @@ import math
 import os
 import asyncio
 
+import requests
+
 from googletrans import Translator
 
 from nltk.corpus import words
@@ -120,10 +122,13 @@ def get_word_score(word : str) -> float :
 def get_random_word(list : list) :
     return "".join(random.sample(list, 1))
 
-# Function retrieving the definition of an English word
-def get_definiton(word : str) -> str :
-    synsets = wn.synsets(word)
-    return synsets[0].definition()
+# Function fetching the definition of a word the free dictionary api
+def get_definition(word : str) -> str:
+    response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    if response.status_code == 200:
+        data = response.json()
+        return data[0]['meanings'][0]['definitions'][0]['definition']
+    return "Definition not found."
 
 # Function that generates a vocabulary bank of commonly used words
 def get_vocabulary(count : int) -> list[str] : 
@@ -133,7 +138,7 @@ def get_vocabulary(count : int) -> list[str] :
         word = random.sample(common_words, 1)[0]
         # Filtering out words with a word score above 5.3 -- words above that threshhold are of uncommon usage
         # print(word, get_word_score(word))
-        if get_word_score(word) < 5.1 and len(word) > 3 and word not in output :
+        if get_word_score(word) < 5.3 and len(word) > 3 and word not in output :
             output.append(word)
             i += 1
     return output
@@ -149,27 +154,27 @@ def build_vocab(vocab : list[str], target : str) -> str:
         output = "\n### Vocabulary \nHere are the words that will be covered in this worksheet. \n|Word|Translation|Definition|\n|:----|:----|:----|\n"
 
         for word in vocab :
-            row = f'|{asyncio.run(get_translation(word,target))}|{word}|{get_definiton(word)}|\n'
+            row = f'|{asyncio.run(get_translation(word,target))}|{word}|{get_definition(word)}|\n'
             output += row
     else :
 
         output = "\n### Vocabulary \nHere are the words that will be covered in this worksheet. \n|Word|Pronounciation|Translation|Definition|\n|:----|:----|:----|:----|\n"
 
         for word in vocab :
-            row = f'|{asyncio.run(get_translation(word,target))}|{asyncio.run(get_pronunciation(word, target=target))}|{word}|{get_definiton(word)}|\n'
+            row = f'|{asyncio.run(get_translation(word,target))}|{asyncio.run(get_pronunciation(word, target=target))}|{word}|{get_definition(word)}|\n'
             output += row
 
     return output
 
 # Writes a sentence containing a specific word
-def write_paragraph(word : list[str], lang : str) -> str :
-        prompt = f'Write a sentence in {lang}, for each of the following words {word}'
+def write_paragraph(vocab : list[str], lang : str) -> str :
+        prompt = f'Write a sentence in {lang}, for each of the following words {vocab}'
         response = (chat_session.send_message(prompt)).text
         return response
 
-def build_fill_in_the_blank(vocab : list[str], paragraph : list[str]) -> str :
-
-    sentences = paragraph
+# Functin parsing the gemini outputs
+def parse_gemini_output(paragraph : str) :
+    sentences = paragraph.split("\n")
     filtered = []
     parsed = []    
     for sentence in sentences :
@@ -180,42 +185,78 @@ def build_fill_in_the_blank(vocab : list[str], paragraph : list[str]) -> str :
         parsed.append(word.split('*'))
 
     parsed = [[word for word in element if word != '' and word != ' '] for element in parsed]
-    print(parsed)
     parsed = [element[-1] for element in parsed if len(element) > 1 ]
-    print(parsed)
+
+    return parsed
+
+# Function building fill in the blank exercices
+def build_fill_in_the_blank(vocab : list[str], target : str) -> str :
+
+    translated_words = [asyncio.run(get_translation(word, target)) for word in vocab]
+    paragraph = write_paragraph(translated_words, target)
+
+    parsed = parse_gemini_output(paragraph)
     
     blanks = []
     for i in range(len(parsed)) :
-        blanks.append(parsed[i].split(vocab[i]))
+        blanks.append(parsed[i].split(translated_words[i]))
     
     parsed = [" ________________ ".join(element) for element in blanks]
     random.shuffle(parsed)
 
-    output = "\n### Fill in the blank \nIn this exercice, fill in the blanks in each sentence with the appropriate vocabulary word from the word bank\n\n"
+    output = "\n### Fill in the blank \nIn this exercice, fill in the blanks in each sentence with the appropriate vocabulary word from the word bank.\n\n"
     for i in range(len(parsed)) :
         output += f'{i+1}. {parsed[i]}\n \n \n' 
 
     return output
 
-# TODO : create a function that will generate a translation exercice
-def build_translation(lang : str, level : float, len : int, vocabulary : list[str]) -> str :
-    pass
+# Function building translation matching exercice
+def build_translation(vocab : list, target : str) -> str :
+    output = "\n### Translation matching \nIn this exercice, match the expression to its appropriate translation.\n\n"
 
-def generate_doc(lang : str, len : int) :
+    paragraph = write_paragraph(vocab, target)
+    parsed = parse_gemini_output(paragraph)
+    parsed = [ element.split("(")[0] for element in parsed ]
+    translated = []
+    pronunciation = []
+
+    test_prn = asyncio.run(get_pronunciation(vocab[0],target=target))
+
+    for sentence in parsed :
+    # Translate the word asynchronously
+        translated.append(asyncio.run(get_translation(sentence, 'en')))
+        if test_prn != None : 
+            pronunciation.append(asyncio.run(get_pronunciation(sentence,target)))
+
+    random.shuffle(translated)
+
+    if test_prn == None :
+        output += '|Sentences|Translations|\n|:----|:----|\n'
+        for i in range(len(parsed)) :
+            output += f'|{parsed[i]}|{translated[i]}|\n'
+    else :
+        output += '|Sentences||Translations|\n|:----|:----|:----|\n'
+        for i in range(len(parsed)) :
+            output += f'|{parsed[i]}|{pronunciation[i]}|{translated[i]}|\n'
+
+    return output
+
+# Function building the mardown file (worksheet) containing the exercices
+def generate_doc(target : str, len : int) :
     directory = "worksheets"
     all_items = os.listdir(directory)
     file_count = sum(1 for item in all_items if os.path.isfile(os.path.join(directory, item)))
     
+    print("Generating Worksheet ...")
+
     output = "# Worksheet"
     vocabulary = get_vocabulary(len)
 
-    output += build_vocab(vocabulary, lang)
-    translated_words = [asyncio.run(get_translation(word, lang)) for word in vocabulary]
-    paragraph = write_paragraph(translated_words, lang)
-    sentences = paragraph.split("\n")
+    output += build_vocab(vocabulary, target)
+    output += build_fill_in_the_blank(vocabulary, target)
+    output += build_translation(vocabulary, target)
 
-    output += build_fill_in_the_blank(translated_words, sentences)
-    print(output)
-    
     with open(f'worksheets/sheet_{file_count+1}.md', 'w', encoding='utf-8') as file :
         file.write(output)
+
+    print("Worksheet generated !")
